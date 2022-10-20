8d92499c764ab21091d38e96dcdfede18666064a
LongPT17 <thanhlong6899@gmail.com>
2022-10-20 22:35:34 +0700

    dev: add Homepage, Error, Checkout, About and fix hot reload page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="about-img" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline" />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            molestiae expedita porro reprehenderit odio, libero repellat
+            incidunt alias tempora excepturi vel obcaecati fuga. Consequuntur,
+            culpa odio ipsa sequi fugiat obcaecati?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +52,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
